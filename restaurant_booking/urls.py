5ae324a47da34f85f1391fe1f49e0c5c7dd1713e
aaronodonoghue96 <aaronodonoghue2k11@gmail.com>
2025-01-30 22:20:31 +0000

    Add urls

---
+++ restaurant_booking/urls.py
@@ -0,0 +1,8 @@
+# restaurant_booking/urls.py
+from django.contrib import admin
+from django.urls import path, include  # include is used to refer to the app-level URLs
+
+urlpatterns = [
+    path('admin/', admin.site.urls),  # Admin page URL
+    path('', include('bookings.urls')),  # Include the app-level URLs from the 'bookings' app
+]
