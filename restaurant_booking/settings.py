1ddf7e5270c326243f6a38dd341c787acb32aff5
aaronodonoghue96 <aaronodonoghue2k11@gmail.com>
2025-02-04 22:37:35 +0000

    Resolve django issue with static root

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
