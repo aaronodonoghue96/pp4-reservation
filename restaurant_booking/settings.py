0f72c7b99225a6a46b79c39eca6bf0f6844e3ddc
aaronodonoghue96 <aaronodonoghue2k11@gmail.com>
2025-03-24 19:43:50 +0000

    Set DB to Postgres on Heroku

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import dj_database_url
+
 from pathlib import Path
 import os
 
@@ -80,10 +82,7 @@ WSGI_APPLICATION = 'restaurant_booking.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 
