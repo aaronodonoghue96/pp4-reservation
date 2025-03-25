09975f9e9962921c985fb94038d1b293dafbbc1e
aaronodonoghue96 <aaronodonoghue2k11@gmail.com>
2025-03-25 09:46:05 +0000

    Added whitenoise for static files

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -18,6 +18,15 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# This line tells Django to look for static files in the 'static' folder in the project root.
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 SECRET_KEY = os.environ["SECRET_KEY"]
@@ -46,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,12 +134,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
